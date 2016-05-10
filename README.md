cd8f1841d53727d40e2456b951fc689398e3c72e
zhanghua <hua.zhang@boyuanitsm.com>
2016-05-10 16:46:14 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fort sdk
+
+hello world
---
+++ src/main/java/com/boyuanitsm/fortsdk/Main.java
@@ -0,0 +1,11 @@
+package com.boyuanitsm.fortsdk;
+
+/**
+ * @author zhanghua on 5/10/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
