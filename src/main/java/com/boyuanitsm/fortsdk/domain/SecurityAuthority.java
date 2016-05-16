1db2476cbdb7d1d71aa5527f85573df355360cce
zhanghua <hua.zhang@boyuanitsm.com>
2016-05-16 15:10:47 +0800

    add domain

---
+++ src/main/java/com/boyuanitsm/fortsdk/domain/SecurityAuthority.java
@@ -0,0 +1,93 @@
+package com.boyuanitsm.fortsdk.domain;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * A SecurityAuthority.
+ */
+public class SecurityAuthority extends AbstractAuditingEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private String st;
+
+    private SecurityApp app;
+
+    private Set<SecurityResourceEntity> resources = new HashSet<SecurityResourceEntity>();
+
+    private Set<SecurityRole> roles = new HashSet<SecurityRole>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSt() {
+        return st;
+    }
+
+    public void setSt(String st) {
+        this.st = st;
+    }
+
+    public SecurityApp getApp() {
+        return app;
+    }
+
+    public void setApp(SecurityApp securityApp) {
+        this.app = securityApp;
+    }
+
+    public Set<SecurityResourceEntity> getResources() {
+        return resources;
+    }
+
+    public void setResources(Set<SecurityResourceEntity> securityResourceEntities) {
+        this.resources = securityResourceEntities;
+    }
+
+    public Set<SecurityRole> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<SecurityRole> securityRoles) {
+        this.roles = securityRoles;
+    }
+
+    @Override
+    public String toString() {
+        return "SecurityAuthority{" +
+            "id=" + id +
+            ", name='" + name + "'" +
+            ", description='" + description + "'" +
+            ", st='" + st + "'" +
+            '}';
+    }
+}
---
+++ src/main/java/com/boyuanitsm/fortsdk/domain/enumeration/ResourceEntityType.java
@@ -0,0 +1,8 @@
+package com.boyuanitsm.fortsdk.domain.enumeration;
+
+/**
+ * The ResourceEntityType enumeration.
+ */
+public enum ResourceEntityType {
+    FUNCTION,NAV
+}
